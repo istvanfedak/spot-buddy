3a5119eaaec19c365bff52acde44dfa8fa8ff2b7
Greg <gxp174@miami.edu>
2019-02-21 13:55:17 -0500

    Building Login Page

---
+++ lib/login_page.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() => new _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+
+  String _email;
+  String _password;
+
+  void validateAndSave() {
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter login demo'),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+        child: new Form(
+            child: new Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children:<Widget>[
+                new TextFormField(
+                  decoration: new InputDecoration(labelText: 'Email'),
+                  validator: (value) => value.isEmpty ? 'Email can\'t be empty' : null
+                ),
+                new TextFormField(
+                  decoration: new InputDecoration(labelText: 'Password'),
+                  validator: (value) => value.isEmpty ? 'Password can\'t be empty' : null
+                  obscureText: true,
+                ),
+                new RaisedButton(
+                  child: new Text('Login',style: new TextStyle(fontSize: 20.0)),
+                  onPressed: validateAndSave,
+                ),
+              ],
+            ),
+        ),
+      ),
+    );
+  }
+}
+
