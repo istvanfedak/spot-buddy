4990c7e7acb6ad30d4c3c468de9e9b78bf574946
Istvan von Fedak <istvan.vonfedak@gmail.com>
2019-02-22 11:22:47 -0500

    Working in group to get the login done. switching to ios for auth.

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -7,12 +7,19 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final formKey = GlobalKey<FormState>();
 
   String _email;
   String _password;
 
   void validateAndSave() {
-
+    final form = formKey.currentState;
+    if(form.validate()) {
+      form.save();
+      print('Form is valid. Email $_email, password $_password');
+    } else {
+      print('Form not invalid. Email $_email, password $_password');
+    }
   }
 
   @override
@@ -24,17 +31,20 @@ class _LoginPageState extends State<LoginPage> {
       body: Container(
         padding: EdgeInsets.all(16.0),
         child: new Form(
+            key: formKey,
             child: new Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children:<Widget>[
                 new TextFormField(
                   decoration: new InputDecoration(labelText: 'Email'),
-                  validator: (value) => value.isEmpty ? 'Email can\'t be empty' : null
+                  validator: (value) => value.isEmpty ? 'Email can\'t be empty' : null,
+                  onSaved: (value)=> _email = value , // save email
                 ),
                 new TextFormField(
                   decoration: new InputDecoration(labelText: 'Password'),
-                  validator: (value) => value.isEmpty ? 'Password can\'t be empty' : null
+                  validator: (value) => value.isEmpty ? 'Password can\'t be empty' : null,
                   obscureText: true,
+                  onSaved: (value) => _password = value ,
                 ),
                 new RaisedButton(
                   child: new Text('Login',style: new TextStyle(fontSize: 20.0)),
