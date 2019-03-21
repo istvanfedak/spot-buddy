b67df77088722a7a6d44ba4ae54bd1ad01298511
David Elimelech <david.elimelech9@gmail.com>
2019-03-21 15:29:18 -0400

    implemented basic google map into feed

--- lib/feed.dart
+++ lib/feed.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:async';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 
 
@@ -10,6 +12,15 @@ class Feed extends StatefulWidget{
 }
 
 class _Feed extends State<Feed> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  static const LatLng _center = const LatLng(45.521563, -122.677433);
+  //need to pull this geolocation from the user, and hopefully show buddies around him 
+
+  void _onMapCreated(GoogleMapController controller) {
+    _controller.complete(controller);
+  }
+
   @override
   Widget build(BuildContext context) {
       return new Scaffold(
@@ -17,6 +28,13 @@ class _Feed extends State<Feed> {
           title: new Text('Buddy Feed')
        ),
         //child: ListPage()
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 11.0,
+          ),
+        ),
       );
   }
 }
